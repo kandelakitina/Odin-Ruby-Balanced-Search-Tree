8f46768094bcc0bca9a42763eea28dc3a21c9c55
boticelli <kandelakitina@gmail.com>
2025-07-25 16:35:38 +0300

    Chores

--- lib/binary_search_tree.rb
+++ lib/binary_search_tree.rb
@@ -9,11 +9,28 @@ class Tree
     @root = build_tree(array)
   end
 
-  def build_tree(array)
-    node = nil
-    array.each do |item|
-      node = insert(node, item)
+  def insert(node, value)
+    return TreeNode.new(value) if node.nil?
+
+    if value < node.value
+      node.left = insert(node.left, value)
+    elsif value > node.value
+      node.right = insert(node.right, value)
     end
+    node
+  end
+
+  def delete(node, target)
+    return nil if node.nil?
+
+    if target < node.value
+      node.left = delete(node.left, target)
+    elsif target > node.value
+      node.right = delete(node.right, target)
+    else
+      node = delete_node(node)
+    end
+
     node
   end
 
@@ -26,54 +43,43 @@ class Tree
     result
   end
 
-  # rubocop:disable Metrics/MethodLength, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
-  def delete(node, target)
-    return nil if node.nil?
+  def pretty_print(node: @root, prefix: '', is_left: true)
+    return if node.nil?
 
-    if target < node.value
-      node.left = delete(node.left, target)
-    elsif target > node.value
-      node.right = delete(node.right, target)
-    elsif node.left.nil? && node.right.nil?
-      # Node to delete found
-      return nil
-    elsif node.left.nil?
-      return node.right
-    elsif node.right.nil?
-      return node.left
-    else
-      # Two children
-      successor = find_successor(node.right)
-      node.value = successor.value
-      node.right = delete(node.right, successor.value)
+    pretty_print(node: node.right, prefix: "#{prefix}#{is_left ? '│   ' : '    '}", is_left: false)
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.value}"
+    pretty_print(node: node.left, prefix: "#{prefix}#{is_left ? '    ' : '│   '}", is_left: true)
+  end
+
+  private
+
+  def build_tree(array)
+    node = nil
+    array.each do |item|
+      node = insert(node, item)
     end
-
     node
   end
-  # rubocop:enable Metrics/MethodLength, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
+
+  def delete_node(node)
+    return nil if leaf?(node)
+    return node.right if node.left.nil?
+    return node.left if node.right.nil?
+
+    # Node has two children
+    successor = find_successor(node.right)
+    node.value = successor.value
+    node.right = delete(node.right, successor.value)
+    node
+  end
+
+  def leaf?(node)
+    node.left.nil? && node.right.nil?
+  end
 
   def find_successor(node)
     return node if node.left.nil?
 
     find_successor(node.left)
-  end
-
-  def insert(node, value)
-    return TreeNode.new(value) if node.nil?
-
-    if value < node.value
-      node.left = insert(node.left, value)
-    elsif value > node.value
-      node.right = insert(node.right, value)
-    end
-    node
-  end
-
-  def pretty_print(node = @root, prefix = '', is_left = true)
-    return if node.nil?
-
-    pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false)
-    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.value}"
-    pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true)
   end
 end
