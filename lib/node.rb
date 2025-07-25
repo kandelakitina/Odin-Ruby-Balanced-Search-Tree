a1f86212a895d3f27b3f44f18a78bc9604b03261
boticelli <kandelakitina@gmail.com>
2025-07-25 10:23:04 +0300

    Add comparable to the node class

--- lib/node.rb
+++ lib/node.rb
@@ -2,11 +2,17 @@
 
 # Node class for out Binary Search Tree
 class Node
+  include Comparable
+
   attr_accessor :left, :right, :value
 
   def initialize(value)
     @left = nil
     @right = nil
     @value = value
+  end
+
+  def <=>(other)
+    value <=> other.value
   end
 end
