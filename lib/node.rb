52bd0d3033860ebc4b9edcfb916d2ac9ef20db5d
boticelli <kandelakitina@gmail.com>
2025-07-25 10:15:24 +0300

    Add node class

---
+++ lib/node.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+# Node class for out Binary Search Tree
+class Node
+  attr_accessor :left, :right, :value
+
+  def initialize(value)
+    @left = nil
+    @right = nil
+    @value = value
+  end
+end
