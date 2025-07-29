e83c41d4164b1d15b5ffc544c0d59bfb4e236165
boticelli <kandelakitina@gmail.com>
2025-07-29 12:06:18 +0300

    Fix insert

--- lib/binary_search_tree.rb
+++ lib/binary_search_tree.rb
@@ -9,13 +9,17 @@ class Tree
     @root = build_tree(array.uniq.sort)
   end
 
-  def insert(value, node = @root)
+  def insert(value)
+    @root = insert_recursive(@root, value)
+  end
+
+  def insert_recursive(node, value)
     return TreeNode.new(value) if node.nil?
 
     if value < node.value
-      node.left = insert(value, node.left)
+      node.left = insert_recursive(node.left, value)
     elsif value > node.value
-      node.right = insert(value, node.right)
+      node.right = insert_recursive(node.right, value)
     end
     node
   end
